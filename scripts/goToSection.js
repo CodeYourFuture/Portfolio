a1337a469b9fdb0aacb838f4dc70309c96462a8c
Pedro <riccip@outlook.com>
2023-11-28 11:15:29 +0000

    console.log deleted from goToSection.js

--- scripts/goToSection.js
+++ scripts/goToSection.js
@@ -1,11 +1,8 @@
 function goToSection(event) {
   event.preventDefault();
-
+  
   const sectionId = event.target.getAttribute("href");
-  console.log(sectionId);
-
   const section = document.querySelector(sectionId);
-  console.log(section);
 
   section.scrollIntoView({
     behavior: "smooth",
